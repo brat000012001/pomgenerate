ad3396ecb4364a2b8e3412fff47cf238cbea8f09
Peter Nalyvayko <brat000012001@gmail.com>
2020-05-17 11:34:18 -0400

    Refactored the project structure and fixed a few formatting issues

--- pomgenerate.py
+++ pomgenerate.py
@@ -12,14 +12,26 @@
 #
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .maven import RepositoryService
-from .maven import MavenPom
+from maven import RepositoryService
+from maven import MavenPom
 import argparse
 
 
 def main(params):
-
     svc = RepositoryService('https://search.maven.org/solrsearch/select')
 
     packages = svc.search(params.group, params.version)
@@ -27,8 +39,8 @@ def main(params):
     pom = MavenPom('com.singularix', 'pom-generate')
     print(pom.build(packages))
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-g',
                         '--group',
